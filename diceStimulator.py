7d1c303b126c33d6b5225ecd58f1a426ce644377
Divya Sanghi <divyasanghi@Divyas-MacBook-Pro.local>
2017-09-14 00:21:16 +0530

    giving spaces while presentation

--- diceStimulator.py
+++ diceStimulator.py
@@ -1,10 +1,10 @@
 from random import *
 
 print randint(1,6)
-x = raw_input("Want to roll again?")
+x = raw_input("Want to roll again? ")
 while x=="yes":
       print randint(1,6)
-      x = raw_input("Want to roll again?")
+      x = raw_input("Want to roll again? ")
 
 
 
